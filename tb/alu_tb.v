4b500802e0ccd590c50b67f940ee4f2257c153dc
Animore-107 <moreaniruddha402@gmail.com>
2026-05-06 10:15:07 +0530

    Update alu_tb.v

--- tb/alu_tb.v
+++ tb/alu_tb.v
@@ -1,1 +1,21 @@
+module alu_tb;
+reg [31:0] a, b;
+reg [2:0] sel;
+wire [31:0] out;
 
+alu uut(a, b, sel, out);
+
+initial begin
+    $dumpfile("dump.vcd");
+    $dumpvars(0, alu_tb);
+
+    a = 10; b = 5;
+
+    sel = 3'b000; #10;
+    sel = 3'b001; #10;
+    sel = 3'b010; #10;
+    sel = 3'b011; #10;
+
+    $finish;
+end
+endmodule
