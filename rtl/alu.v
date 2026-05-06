9ce19a721b99e04f13a365687c5492f2ae2e8da3
Animore-107 <moreaniruddha402@gmail.com>
2026-05-06 10:13:30 +0530

    Update alu.v

--- rtl/alu.v
+++ rtl/alu.v
@@ -1,1 +1,11 @@
-
+module alu(input [31:0] a, b, input [2:0] sel, output reg [31:0] out);
+always @(*) begin
+    case(sel)
+        3'b000: out = a + b;
+        3'b001: out = a - b;
+        3'b010: out = a & b;
+        3'b011: out = a | b;
+        default: out = 0;
+    endcase
+end
+endmodule
